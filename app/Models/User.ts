bd9aa2c9e04cbd9abc81ceb38ee4022f377aca5b
Matteo C <hello@micorksen.eu>
2023-05-18 19:16:00 +0000

    Part 1: Authentication and some Part 2: Posts System

---
+++ app/Models/User.ts
@@ -0,0 +1,40 @@
+import { DateTime } from 'luxon'
+import Hash from '@ioc:Adonis/Core/Hash'
+import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
+
+enum UserPermissions {
+  User = 1,
+  Blogger = 2,
+  Moderator = 3,
+  Admin = 4
+}
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public username: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column({ serializeAs: null })
+  public rememberMeToken: string | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeSave()
+  public static async hashPassword (user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+}
