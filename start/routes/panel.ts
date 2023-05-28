66a1966031db64a1c605e372d1a0297883ae7261
Mael <mael@pop-os.localdomain>
2023-05-28 13:15:31 +0200

    fix

--- start/routes/panel.ts
+++ start/routes/panel.ts
@@ -23,12 +23,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('/', 'PanelController.list')
   Route.get(':slug', 'PostsController.get')
-
-  Route.group(() => {
-    Route.post('/', 'PostsController.new')
-    Route.put(':slug', 'PostsController.update')
-    Route.delete(':slug', 'PostsController.delete')
-
-    Route.post(':slug/comment', 'CommentsController.new')
-  }).middleware('auth')
-}).prefix('/panel')
+})
+  .middleware('auth')
+  .prefix('/panel')
