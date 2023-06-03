d9f48e0c309bb7bec24cbb10e96eb4c3cbd83ece
Mael <Eragon941@outlook.fr>
2023-06-03 13:08:49 +0200

    fix bug

--- start/routes/panel.ts
+++ start/routes/panel.ts
@@ -23,4 +23,5 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.get('/', 'PanelController.list')
   Route.get(':slug', 'PostsController.get')
+  Route.get('iswriter', 'UsersController.iswriter')
 }).prefix('/panel')
