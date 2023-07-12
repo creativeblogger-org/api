da59c10ca076bdac0d3280e2508ad0d3c946500b
RIC217 <melericcardo9400@gmail.com>
2023-07-12 16:47:17 +0200

    Patched CORS error

--- app/Services/Ws.ts
+++ app/Services/Ws.ts
@@ -1,18 +1,22 @@
-import { Server } from 'socket.io';
-import AdonisServer from '@ioc:Adonis/Core/Server';
+import { Server } from 'socket.io'
+import AdonisServer from '@ioc:Adonis/Core/Server'
 
 class Ws {
-    public io: Server
-    private booted = false
+  public io: Server
+  private booted = false
 
-    public boot() {
-        if (this.booted) {
-            return
-        }
-
-        this.booted = true
-        this.io = new Server(AdonisServer.instance!)
+  public boot() {
+    if (this.booted) {
+      return
     }
+
+    this.booted = true
+    this.io = new Server(AdonisServer.instance!, {
+      cors: {
+        origin: '*',
+      },
+    })
+  }
 }
 
 export default new Ws()
