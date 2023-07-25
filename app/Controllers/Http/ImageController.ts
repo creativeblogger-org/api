2306f43850018ecd21d0e9290baf92f5e88b4f0b
Mael <Eragon941@outlook.fr>
2023-07-25 08:58:51 +0200

    fix

--- app/Controllers/Http/ImageController.ts
+++ app/Controllers/Http/ImageController.ts
@@ -1,6 +1,7 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Application from '@ioc:Adonis/Core/Application'
 import fs from 'fs/promises'
+import APIException from 'App/Exceptions/APIException'
 
 export default class ImageController {
   public async upload({ request, response }: HttpContextContract) {
@@ -8,38 +9,37 @@ export default class ImageController {
     const name = request.param('id')
 
     if (!image) {
-      return response.badRequest('No image file uploaded')
+      throw new APIException("Il n'y a aucun fichier à télécharger", 404)
     }
 
     const fileName = `${name}.${image.extname}`
     const path = `uploads/${fileName}`
 
     try {
-      await image.move(Application.publicPath(path))
-
-      if (!image.isValid) {
-        return response.badRequest(image.errors)
-      }
+      await image.move(Application.publicPath(), {
+        name: fileName,
+        overwrite: true, // Cette option permettra de remplacer le fichier s'il existe déjà
+      })
 
       return response.ok({ path })
     } catch (error) {
-      return response.internalServerError('Failed to upload image')
+      throw new APIException("Erreur durant l'upload", 501)
     }
   }
 
-  public async show({ params, response }: HttpContextContract) {
-    const imageName = params.imageName
+  public async show({ request, response }: HttpContextContract) {
+    const imageName = request.param('imageName')
 
     try {
       // Vérifiez si le fichier existe avant de renvoyer une réponse
-      const imagePath = Application.publicPath(`uploads/${imageName}/${imageName}`)
+      const imagePath = Application.publicPath(`${imageName}` + '.png')
       await fs.access(imagePath)
 
       // Renvoyer le fichier image si tout est bon
       return response.download(imagePath)
     } catch (error) {
       // Si le fichier n'existe pas ou s'il y a une autre erreur, renvoyer une réponse 404
-      return response.notFound()
+      throw new APIException("L'image n'a pas été trouvée...", 404)
     }
   }
 }
