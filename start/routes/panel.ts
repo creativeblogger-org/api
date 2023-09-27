904c5158e916ce22e5baf85e7969fb0dcfbcc7be
Mael <Eragon941@outlook.fr>
2023-09-27 18:00:52 +0200

    fix bug

--- start/routes/panel.ts
+++ start/routes/panel.ts
@@ -2,8 +2,8 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', 'PanelController.list')
-  Route.get('/banner', 'PanelController.banner')
-  Route.get('/delete', 'PanelController.deleteBanner')
+  Route.post('/banner', 'PanelController.banner')
+  Route.delete('/delete', 'PanelController.deleteBanner')
   Route.get(':slug', 'PostsController.get')
   Route.get('iswriter', 'UsersController.iswriter')
 }).prefix('/panel')
