6cdf936b835bd27c0b442f961de0e4fbe668cf6e
SquareDot 3301 <mael.cherigui@proton.me>
2023-11-25 17:23:29 +0100

    RSS update to Feed package

--- app/Services/RssGenerator.ts
+++ app/Services/RssGenerator.ts
@@ -1,42 +1,60 @@
-import Post from 'App/Models/Post';
-import { promises as fsPromises } from 'fs';
-import path from 'path';
+import Post from 'App/Models/Post'
+import { promises as fsPromises } from 'fs'
+import path from 'path'
+import { Feed } from 'feed'
+import { Marked } from '@ts-stack/markdown'
 
-export default class RssGenerator {      
-  public generateRss(posts: Post[]): string {
-    const rssItems = posts
-      .map((post) => {
-        const item = `
-          <item>
-            <title>${post.title}</title>
-            <link>https://creativeblogger.org/posts/${post.slug}</link>
-            <description>${post.description}</description>
-            <pubDate>${post.createdAt}</pubDate>
-            <author>${post.author.username}</author>
-          </item>
-        `;
-        return item;
+export default class RssGenerator {
+  private convertMarkdownToHtml(content: string): string {
+    return Marked.parse(content)
+  }
+
+  public generateRss(posts: Post[]) {
+    const feed = new Feed({
+      title: 'Creative Blogger',
+      description: 'Projet collaboratif entre bloggers',
+      id: 'https://creativeblogger.org',
+      link: 'https://creativeblogger.org',
+      language: 'fr',
+      image: 'https://creativeblogger.org/assets/logo2-0c8c0aee.png',
+      favicon: 'https://creativeblogger.org/assets/logo2-0c8c0aee.png',
+      copyright: 'Copyleft, you can use the content',
+      updated: new Date(2023, 11, 25),
+      generator: 'awesome',
+      feedLinks: {
+        json: 'https://example.com/json',
+        atom: 'https://example.com/atom',
+      },
+      author: {
+        name: 'Creative Blogger Org',
+        email: 'contact@creativeblogger.org',
+        link: 'https://creativeblogger.org',
+      },
+    })
+    posts.forEach((post) => {
+      const htmlContent = this.convertMarkdownToHtml(post.content)
+      feed.addItem({
+        title: post.title,
+        id: `https://creativeblogger.org/posts/${post.slug}`,
+        link: `https://creativeblogger.org/posts/${post.slug}`,
+        description: post.description,
+        content: htmlContent,
+        author: [
+          {
+            name: post.author.username,
+            link: `https://creativeblogger.org/users/${post.author.username}`,
+          },
+        ],
+        date: new Date(),
+        image: post.image,
       })
-      .join('');
-
-    const rss = `
-      <rss version="2.0">
-        <channel>
-          <title>CreativeBlogger</title>
-          <link>https://creativeblogger.org</link>
-          <description>Coopérativité de blogger</description>
-          <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
-          ${rssItems}
-        </channel>
-      </rss>
-    `;
-
-    return rss;
+    })
+    return feed.rss2()
   }
 
   public async saveRssToFile(rss: string): Promise<void> {
-    const rssFilePath = path.join(__dirname, '../../rss.xml');
+    const rssFilePath = path.join(__dirname, '../../rss.xml')
 
-    await fsPromises.writeFile(rssFilePath, rss, 'utf-8');
+    await fsPromises.writeFile(rssFilePath, rss, 'utf-8')
   }
 }
