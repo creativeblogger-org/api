5c65e75d66e9ddb6176c520c67fb58376985dfbb
SquareDot 3301 <mael.cherigui@proton.me>
2023-11-25 14:48:12 +0100

    Add markdown to rss feed

--- app/Services/RssGenerator.ts
+++ app/Services/RssGenerator.ts
@@ -1,24 +1,30 @@
-import Post from 'App/Models/Post';
-import { promises as fsPromises } from 'fs';
-import path from 'path';
+import Post from 'App/Models/Post'
+import { promises as fsPromises } from 'fs'
+import path from 'path'
+import marked from 'marked'
 
 export default class RssGenerator {
+  private convertMarkdownToHtml(content: string): string {
+    return marked(content)
+  }
+
   public generateRss(posts: Post[]): string {
     const rssItems = posts
       .map((post) => {
+        const htmlContent = this.convertMarkdownToHtml(post.content)
         const item = `
           <item>
             <title>${post.title}</title>
             <link>https://creativeblogger.org/posts/${post.slug}</link>
             <description>${post.description}</description>
-            <description>${post.content}</description>
-            <pubDate>${new Date().toUTCString()}</pubDate>
+            <description>${htmlContent}</description>
+            <pubDate>${post.createdAt}</pubDate>
             <author>${post.author.username}</author>
           </item>
-        `;
-        return item;
+        `
+        return item
       })
-      .join('');
+      .join('')
 
     const rss = `
       <rss version="2.0">
@@ -30,14 +36,14 @@ export default class RssGenerator {
           ${rssItems}
         </channel>
       </rss>
-    `;
+    `
 
-    return rss;
+    return rss
   }
 
   public async saveRssToFile(rss: string): Promise<void> {
-    const rssFilePath = path.join(__dirname, '../../rss.xml');
+    const rssFilePath = path.join(__dirname, '../../rss.xml')
 
-    await fsPromises.writeFile(rssFilePath, rss, 'utf-8');
+    await fsPromises.writeFile(rssFilePath, rss, 'utf-8')
   }
 }
