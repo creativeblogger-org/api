1460dde4f6ccdda34cbebe509eeeecc435aa0db0
SquareDot 3301 <mael.cherigui@proton.me>
2024-01-22 14:59:46 +0100

    Fix bug

--- start/routes/global.ts
+++ start/routes/global.ts
@@ -3,6 +3,6 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.group(() => {
     Route.post('/ask-certif/:slug', 'GlobalController.ask_certif')
-    Route.post('redactor-list', 'GlobalController.list')
+    Route.get('redactor-list', 'GlobalController.list')
   }).middleware('auth')
 }).prefix('/global')
